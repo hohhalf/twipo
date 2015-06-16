151211d4dc6e2f7cb08f2e60aaf84197a125ab78
hohhalf <hohhalf@gmail.com>
2015-06-16 10:43:43 +0300

    created twips cast page

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,10 +6,13 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Twip;
 
 class MainController extends Controller
 {
-    public function welcome(){
-        return view("welcome");
+    public function welcome()
+    {
+        $twips = Twip::all();
+        return view("welcome", compact('twips'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,42 +2,20 @@
 <html>
     <head>
         <title>Twipo</title>
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: darkslategrey;
-                display: table;
-                font-weight: 100;
-                font-family: 'Arial', serif;
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 24px;
-                font-weight: bolder;
-            }
-
-        </style>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Twipo</div>
+                <div class="title"><h1>Twipo</h1></div>
+                 <hr>
+                @foreach($twips as $twip)
+                  <article>
+                      <h2>{{$twip->title}}</h2>
+                      <div class="body"><h3>{{$twip->twip}}</h3></div>
+                  </article>
+                @endforeach
+                </table>
             </div>
         </div>
     </body>
